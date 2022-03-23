52418d302730c9009f492e990aec266cd7eb8483
FlutLab.io <>
2022-03-23 10:13:38 +0000

    Autosave

---
+++ lib/components/bmi_result.dart
@@ -0,0 +1,7 @@
+class BmiResult {
+  final String height;
+  final String weight;
+  final String bmi;
+
+  const BmiResult(this.height, this.weight, this.bmi);
+}
--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "../components/bmi_result.dart";
 
 class Result extends StatefulWidget {
   const Result({Key key}) : super(key: key);
