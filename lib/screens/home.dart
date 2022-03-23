ab4749a93198e13233039d48cd28fcba62038ef5
FlutLab.io <>
2022-03-23 11:51:58 +0000

    Autosave

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -131,6 +131,7 @@ class _HomeState extends State<Home> {
                         ),
                       ),
                     );
+                    _clearData();
                   }
                 })
         ]))
