719cf62151aabfed3eaa5ec18e4ee864859cf872
FlutLab.io <>
2022-03-23 12:35:04 +0000

    Autosave

--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -6,9 +6,6 @@ class Result extends StatelessWidget {
   final BmiResult bmiResult;
 
   _bmiResultText() {
-    print(bmiResult.getHeight);
-    print(bmiResult.getWeight);
-    print(bmiResult.getBmi);
   }
 
   @override
@@ -17,6 +14,20 @@ class Result extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Result'),
         ),
-        body: Center(child: new RichText(text: TextSpan(text: _bmiResultText()))));
+        body: Center(child: new RichText(
+            text: const TextSpan(children: [
+              TextSpan(
+                  text: 'The dog ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.red)),
+              TextSpan(
+                  text: 'is a domesticated carnivore ',
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic, color: Colors.purple)),
+              TextSpan(
+                  text: 'of the family Canidae.',
+                  style: TextStyle(color: Colors.green))
+            ]),
+          ));
   }
 }
