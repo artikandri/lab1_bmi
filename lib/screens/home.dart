99a0bd4dc055b7015af44902c0d7124b37322ccb
FlutLab.io <>
2022-03-24 00:52:39 +0000

    Autosave

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -60,7 +60,7 @@ class _HomeState extends State<Home> {
               text: TextSpan(
                   text: 'Measurement unit',
                   style: (TextStyle(
-                    color: Theme.of(context).primaryColor,
+                    color: Theme.of(context).textTheme.bodyText,
                     fontSize: fontSize,
                   ))),
             ),
