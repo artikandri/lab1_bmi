e22fe1d6f075ce2984a9f6e4dc5b2d06a0bdfbc0
FlutLab.io <>
2022-03-24 02:48:25 +0000

    Autosave

--- test/ui/screens/authorship_test.dart
+++ test/ui/screens/authorship_test.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter/material.dart';
@@ -6,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:lab1_bmi/screens/authorship.dart' as authorship;
 import 'package:lab1_bmi/styling.dart';
 import 'package:lab1_bmi/constants.dart';
-
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -19,15 +17,13 @@ void main() {
       var appTitle = find.text(Constants.authorship['appTitle']);
       expect(appTitle, findsOneWidget);
 
-
       // Verify the author is available
       var author = find.text(Constants.authorship['author']);
       expect(author, findsOneWidget);
 
-
       // Verify the title is available
       var title = find.text(Constants.authorship['title']);
       expect(title, findsOneWidget);
-
     });
   });
+}
