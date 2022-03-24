89eff231fbb7c9831e7a9c0bbde108eac67db8dd
FlutLab.io <>
2022-03-24 01:05:06 +0000

    Autosave

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -58,6 +58,7 @@ class _HomeState extends State<Home> {
     return new Column(
       children: <Widget>[
         Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             RichText(
